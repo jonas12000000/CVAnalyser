5117dc442eeaae58018210389a6fad86d13be09b
Jonas <jonas12000000@gmail.com>
2025-10-18 00:25:24 -0400

    add authentication

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { Link } from 'react-router'
 import ScoreCircle from './ScoreCircle'
 
-const ResumeCard = ({resume: {id, companyName, jobTitle, feedback}}: {resume: Resume}) => {
+const ResumeCard = ({resume: {id, companyName, jobTitle, feedback, imagePath}}: {resume: Resume}) => {
   return (
     <Link to={`/resume/${id}`} className='resume-card animate-in fade-in duration-1000' >
+      <div className='resume-card-header'>
         <div className='flex flex-col gap-2'>
             <h2 className='!text-black font-bold break-words'>
                 {companyName}
@@ -12,8 +13,18 @@ const ResumeCard = ({resume: {id, companyName, jobTitle, feedback}}: {resume: Re
             <h3 className='text-lg break-words text-gray-500'>{jobTitle}</h3>
         </div>
         <div className='flex-shrink-0'>
-            <ScoreCircle score={25}/>
+            <ScoreCircle score={feedback.overallScore}/>
         </div>
+      </div>
+      <div className='gradient-border animate-in fade-in duration-1000'>
+        <div className='w-full h-full'>
+          <img src={imagePath}
+            alt='resume'
+            className='w-full h-[350px] max-sm:[200px] object-cover object-top'
+          />
+        </div>
+      </div>
+        
     </Link>
   )
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,24 +14,29 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return (
-  <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar />
-    <section className="main-section">
-      <div className="page-heading">
-        <h1>Track your applications and resume rating</h1>
-        <h2>Recieve feedbacks on your submissions and resumes</h2>
-      </div>
-    </section>
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
 
-    {resumes.length > 0 && (
-      <div className="resumes-section">
-        {resumes.map((resume) => (
-          <ResumeCard key={resume.id} resume={resume} />
-        ))}
-      </div>
-    )}
-    
-  </main>
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated]);
+
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track your applications and resume rating</h1>
+          <h2>Recieve feedbacks on your submissions and resumes</h2>
+        </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
   );
 }
